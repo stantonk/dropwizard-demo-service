38da4677995ac7b52a052c8c57d2453e05f61127
Kevin Stanton <kevin@spothero.com>
2016-08-13 14:13:58 -0700

    imports cleanup

--- src/main/java/com/github/stantonk/DemoServiceConfiguration.java
+++ src/main/java/com/github/stantonk/DemoServiceConfiguration.java
@@ -1,12 +1,11 @@
 package com.github.stantonk;
 
-import io.dropwizard.Configuration;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.dropwizard.Configuration;
 import io.dropwizard.db.DataSourceFactory;
-import org.hibernate.validator.constraints.*;
 
 import javax.validation.Valid;
-import javax.validation.constraints.*;
+import javax.validation.constraints.NotNull;
 
 public class DemoServiceConfiguration extends Configuration {
 
